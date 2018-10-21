4a4e7bfd162be67dae8fb566a038264d8dcf08c0
aiyane <2310091880qq@gmail.com>
2018-10-21 15:59:59 +0800

    爬楼梯

---
+++ 51-100/爬楼梯.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 爬楼梯.py
+"""
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+注意：给定 n 是一个正整数。
+
+示例 1：
+输入： 2
+输出： 2
+解释： 有两种方法可以爬到楼顶。
+1.  1 阶 + 1 阶
+2.  2 阶
+
+示例 2：
+输入： 3
+输出： 3
+解释： 有三种方法可以爬到楼顶。
+1.  1 阶 + 1 阶 + 1 阶
+2.  1 阶 + 2 阶
+3.  2 阶 + 1 阶
+"""
+"""
+思路：动态规划，更好的方法如下，为正向的循环。
+def climbStairs(self, n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    res = list()
+    res = [1]
+    if n >= 2:
+        res.append(2)
+    for i in range(2, n):
+        res.append(res[i - 1] + res[i - 2])
+    return res[n - 1]
+"""
+__author__ = 'Aiyane'
+
+
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        self.map = {}
+        return self.start(n)
+
+    def start(self, n):
+        if n in self.map:
+            return self.map[n]
+        if n == 0:
+            return 1
+        num = 0
+        if n > 1:
+            num += self.start(n-1)
+        if n > 2:
+            num += self.start(n-2)
+        self.map[n] = num
+        return num
