b23c5130f413520e0c69050ed3effa0ac7e1c9ee
aiyane <2310091880qq@gmail.com>
2018-10-21 14:22:53 +0800

    二进制求和

---
+++ 51-100/二进制求和.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 二进制求和.py
+"""
+给定两个二进制字符串，返回他们的和（用二进制表示）。
+
+输入为非空字符串且只包含数字 1 和 0。
+
+示例 1:
+输入: a = "11", b = "1"
+输出: "100"
+
+示例 2:
+输入: a = "1010", b = "1011"
+输出: "10101"
+"""
+"""
+思路：如下
+"""
+__author__ = 'Aiyane'
+
+
+class Solution:
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a, 2) + int(b, 2))[2:]
+        
