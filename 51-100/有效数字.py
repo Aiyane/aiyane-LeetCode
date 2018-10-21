92cf356aa1ef7c68f16f0f1eff0fca3168d304ea
aiyane <2310091880qq@gmail.com>
2018-10-21 14:04:04 +0800

    有效数字

---
+++ 51-100/有效数字.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 有效数字.py
+"""
+验证给定的字符串是否为数字。
+
+例如:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+
+说明: 我们有意将问题陈述地比较模糊。在实现代码之前，你应当事先思考所有可能的情况。
+
+更新于 2015-02-10:
+C++函数的形式已经更新了。如果你仍然看见你的函数接收 const char * 类型的参数，请点击重载按钮重置你的代码。
+"""
+"""
+思路：对 python 来讲，这种题没有意义。
+"""
+__author__ = 'Aiyane'
+
+
+class Solution:
+    def isNumber(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        try:
+            float(s)
+            return True
+        except ValueError:
+            return False
+        
