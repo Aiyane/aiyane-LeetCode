3e4eee77e55043ca0ffaedf751fc556d5278f79a
aiyane <2310091880qq@gmail.com>
2018-10-24 20:16:52 +0800

    相同的树

---
+++ 51-100/相同的树.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 相同的树.py
+"""
+给定两个二叉树，编写一个函数来检验它们是否相同。
+如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+
+示例 1:
+输入:       1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+输出: true
+
+示例 2:
+输入:      1          1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+输出: false
+
+示例 3:
+输入:       1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+输出: false
+"""
+__author__ = 'Aiyane'
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        if not p and not q:
+            return True
+        elif not p or not q:
+            return False
+        if p.val == q.val:
+            if p.left and q.left:
+                if not self.isSameTree(p.left, q.left):
+                    return False
+            elif p.left or q.left:
+                return False
+            if p.right and q.right:
+                if not self.isSameTree(p.right, q.right):
+                    return False
+            elif p.right or q.right:
+                return False
+            return True
+        return False
+        
