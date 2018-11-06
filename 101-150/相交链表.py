abd90fa6a577002834315cdbebf2c69002bd1b26
aiyane <2310091880qq@gmail.com>
2018-11-06 21:41:31 +0800

    相交链表

---
+++ 101-150/相交链表.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 相交链表.py
+"""
+编写一个程序，找到两个单链表相交的起始节点。
+
+例如，下面的两个链表：
+
+A:          a1 → a2
+                   ↘
+                     c1 → c2 → c3
+                   ↗            
+B:     b1 → b2 → b3
+在节点 c1 开始相交。
+
+注意：
+
+如果两个链表没有交点，返回 null.
+在返回结果后，两个链表仍须保持原有的结构。
+可假定整个链表结构中没有循环。
+程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+"""
+"""
+思路：循环两遍，一个链表到尾时就从另一个链表的头开始。
+"""
+__author__ = 'Aiyane'
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        """
+        p1, p2 = headA, headB
+        while(p1 != p2):
+            p1 = headB if p1 == None else p1.next
+            p2 = headA if p2 == None else p2.next
+        return p1
