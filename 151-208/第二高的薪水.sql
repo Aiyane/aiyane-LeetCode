3ed9b5e5db8331c99a50a9db47ffda445045eabe
aiyane <2310091880qq@gmail.com>
2018-11-13 19:34:18 +0800

    第二高的薪水

---
+++ 151-208/第二高的薪水.sql
@@ -0,0 +1,29 @@
+/*
+Create table If Not Exists Employee (Id int, Salary int)
+Truncate table Employee
+insert into Employee (Id, Salary) values ('1', '100')
+insert into Employee (Id, Salary) values ('2', '200')
+insert into Employee (Id, Salary) values ('3', '300')
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+*/
+/* 注意写法，使用括号，后缀使用 AS 语句，使用 DISTINCT 语句筛选，使用 DESC 语句，使用 LIMIT 语句。 */
+SELECT (
+    SELECT DISTINCT Salary
+    FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1, 1
+) AS SecondHighestSalary;
