608082ad6886cb6982ca77ce93d3203986870b40
aiyane <2310091880qq@gmail.com>
2018-11-13 13:04:01 +0800

    寻找峰值

---
+++ 151-208/寻找峰值.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 寻找峰值.py
+"""
+峰值元素是指其值大于左右相邻值的元素。
+
+给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+
+数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+
+你可以假设 nums[-1] = nums[n] = -∞。
+
+示例 1:
+输入: nums = [1,2,3,1]
+输出: 2
+解释: 3 是峰值元素，你的函数应该返回其索引 2。
+
+示例 2:
+输入: nums = [1,2,1,3,5,6,4]
+输出: 1 或 5 
+解释: 你的函数可以返回索引 1，其峰值元素为 2；
+     或者返回索引 5， 其峰值元素为 6。
+
+说明:
+你的解法应该是 O(logN) 时间复杂度的。
+"""
+"""
+思路：先是二分法，比较中间数字与前一个大小，若小于，则峰值在前一段，若大于，峰值在后一段，记得序号需要加中间数的序号。
+"""
+__author__ = 'Aiyane'
+
+
+class Solution:
+    def findPeakElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = len(nums) // 2
+        if k == 0: return 0
+        return k + self.findPeakElement(nums[k:]) if nums[k-1] < nums[k] else self.findPeakElement(nums[:k])
