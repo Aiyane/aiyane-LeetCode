dc0d6023327777e73bc46f37c1c0711845ff73fa
aiyane <2310091880qq@gmail.com>
2018-11-18 16:09:27 +0800

    反转链表

---
+++ 151-208/反转链表.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 反转链表.py
+"""
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+进阶:
+你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+"""
+__author__ = 'Aiyane'
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        ptr = head.next
+        head.next = None
+        while ptr:
+            ptr.next, head, ptr = head, ptr, ptr.next
+        return head
