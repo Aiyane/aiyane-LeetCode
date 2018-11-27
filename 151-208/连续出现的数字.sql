5b08e13a95d1976273430aaeb64c0c13807d0fbc
aiyane <2310091880qq@gmail.com>
2018-11-27 13:21:06 +0800

    连续出现的数字

--- 151-208/连续出现的数字.sql
+++ 151-208/连续出现的数字.sql
@@ -29,3 +29,11 @@ insert into Logs (Id, Num) values ('7', '2')
 | 1               |
 +-----------------+
 */
+
+/*
+思路：将表对自身进行三次比较，分别比较 Id 属性和 Num 属性，要求 Id 属性要是连续的，而且 Num 属性要是一样的
+*/
+select distinct l1.Num as ConsecutiveNums
+from Logs l1, Logs l2, Logs l3
+where l1.Id = l2.Id-1 and l2.Id = l3.Id-1
+and l1.Num = l2.Num and l2.Num = l3.Num
