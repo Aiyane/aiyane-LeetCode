b4bc63ef7390be9d8543a9a8a10744db1a071003
aiyane <2310091880qq@gmail.com>
2018-12-08 23:23:08 +0800

    单词模式

---
+++ 289-367/单词模式.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# 单词模式.py
+"""
+给定一种 pattern(模式) 和一个字符串 str ，判断 str 是否遵循相同的模式。
+
+这里的遵循指完全匹配，例如， pattern 里的每个字母和字符串 str 中的每个非空单词之间存在着双向连接的对应模式。
+
+示例1:
+输入: pattern = "abba", str = "dog cat cat dog"
+输出: true
+
+示例 2:
+输入:pattern = "abba", str = "dog cat cat fish"
+输出: false
+
+示例 3:
+输入: pattern = "aaaa", str = "dog cat cat dog"
+输出: false
+
+示例 4:
+输入: pattern = "abba", str = "dog dog dog dog"
+输出: false
+说明:
+你可以假设 pattern 只包含小写字母， str 包含了由单个空格分隔的小写字母。    
+"""
+__author__ = 'Aiyane'
+
+
+class Solution:
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        table = dict()
+        values = set()
+        if len(pattern) != len(str.split(' ')):
+            return False
+        for ch, word in zip(pattern, str.split(' ')):
+            if ch not in table:
+                if word not in values:
+                    table[ch] = word
+                    values.add(word)
+                else:
+                    return False
+            elif table[ch] != word:
+                return False
+        return True
